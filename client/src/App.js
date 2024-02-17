1f8e34064e93ad3c3dcdd5c39b8dd1021e2ec314
Farzine <farzine07@student.sust.edu>
2024-02-17 12:50:00 +0600

    fixed bg img issue

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import Landing from './components/pages/Landing/Landing';
 import Login from './components/pages/Login/Login.jsx';
 import Regpage from './components/pages/Regpage/Regpage.jsx'
+import { BrowserRouter as BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-      <Landing/>
-      
-    </div>
-  );
-}
-
-export default App;
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/regpage" element={<Regpage />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  export default App;
