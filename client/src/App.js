29a352127d502201bdad4f87f9427f29e13c57ec
Farzine <farzine07@student.sust.edu>
2024-02-17 12:01:17 +0600

    asd

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Landing from './components/pages/Landing/Landing';
+import Login from './components/pages/Login/Login.jsx';
+function App() {
+  return (
+    <div>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginCard from './LoginCard.jsx'
+import './Login.css'
+export default function Login() {
+  return (
+    <div className=' flex justify-center items-center h-screen'>
+        <LoginCard/>
+    </div>
+  )
+}
