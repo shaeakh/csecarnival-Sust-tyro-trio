fea8dbd76c1efc81a12522d7e2e3eb262e4d1fce
Farzine <farzine07@student.sust.edu>
2024-02-17 12:12:03 +0600

    Login Page Added

--- client/src/components/pages/Login/LoginCard.jsx
+++ client/src/components/pages/Login/LoginCard.jsx
@@ -3,22 +3,23 @@ import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
 
 export default function LoginCard() {
   return (
-    <form className="flex max-w-md flex-col gap-4">
+    <form className="max-w-sm bg-black bg-opacity-40 p-4 rounded-lg shadow-md" style={{ width: '420px' }}>
+        <h1 className='mb-3 block text-white text-center font-bold text-2xl'>Login</h1>
       <div>
-        <div className="mb-2 block">
-          <Label htmlFor="email1" value="Your email" />
-        </div>
-        <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
+        {/* <div className="mb-2 block">
+          <Label className='text-white ' htmlFor="email1" value="Your email" />
+        </div> */}
+        <TextInput className='mb-3 block' id="email1" type="email" placeholder="Your Email" required />
       </div>
       <div>
-        <div className="mb-2 block">
+        {/* <div className="mb-2 block">
           <Label htmlFor="password1" value="Your password" />
-        </div>
-        <TextInput id="password1" type="password" required />
+        </div> */}
+        <TextInput className='mb-3 block' id="password1" type="Password" placeholder="Password" required />
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 mb-2 block">
         <Checkbox id="remember" />
-        <Label htmlFor="remember">Remember me</Label>
+        <Label className='text-white' htmlFor="remember">Remember me</Label>
       </div>
       <Button type="submit">Submit</Button>
     </form>
